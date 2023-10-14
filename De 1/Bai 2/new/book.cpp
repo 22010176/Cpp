4191133c1a19a272cc93057302a5097e8f16a794
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-10-14 15:19:42 +0700

    ed

--- De 1/Bai 2/new/book.cpp
+++ De 1/Bai 2/new/book.cpp
@@ -1,6 +1,8 @@
 #ifndef _BOOK_H_
 #define _BOOK_H_
 
+class Book {
 
+};
 
 #endif
--- De 1/Bai 2/new/person.cpp
+++ De 1/Bai 2/new/person.cpp
@@ -1,8 +1,15 @@
 #ifndef _PERSON_H_
 #define _PERSON_H_
 
+class Person {
 
+};
 
+class Adult : public Person {
 
+};
+class Children : public Person {
+
+};
 
 #endif
