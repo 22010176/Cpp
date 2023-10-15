9b7fc34fb456f40fd44b98fe96f43035db831087
_____ <22010176@st.phenikaa-uni.edu.vn>
2023-10-15 13:48:42 +0700

    ed

--- De 1/Bai 2/new/book.cpp
+++ De 1/Bai 2/new/book.cpp
@@ -1,8 +1,20 @@
 #ifndef _BOOK_H_
 #define _BOOK_H_
 
-class Book {
+#include <string>
 
+using namespace std;
+
+class Book {
+private:
+  string nameBook, nameAuthor;
+  int numberBook;
+public:
+  Book() {}
+  Book(string nameBook, string nameAuthor, int numberBook) : nameBook(nameBook), nameAuthor(nameAuthor), numberBook(numberBook) {}
+  string getNameBook() { return nameBook; }
+  string getNameAuthor() { return nameAuthor; }
+  int getNumberBook() { return numberBook; }
 };
 
-#endif
+#endif  
--- De 1/Bai 2/new/person.cpp
+++ De 1/Bai 2/new/person.cpp
@@ -2,14 +2,17 @@
 #define _PERSON_H_
 
 class Person {
-
+private:
+public:
 };
 
 class Adult : public Person {
-
+private:
+public:
 };
 class Children : public Person {
-
+private:
+public:
 };
 
 #endif
